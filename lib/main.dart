525080e6cab4b72dd12e3e69e94d6caf425e3bb0
MPadilla198 <miguel.padilla.harmony2013@gmail.com>
2017-04-24 22:50:23 -0500

    Update!

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(new MyApp());
+  runApp(new MaterialApp(
+    title: 'Lists',
+    theme: new ThemeData(
+      // This is the theme of your application.
+      //
+      // Try running your application with "flutter run". You'll see
+      // the application has a blue toolbar. Then, without quitting
+      // the app, try changing the primarySwatch below to Colors.green
+      // and then invoke "hot reload" (press "r" in the console where
+      // you ran "flutter run", or press Run > Hot Reload App in IntelliJ).
+      // Notice that the counter didn't reset back to zero -- the application
+      // is not restarted.
+      primarySwatch: Colors.blue,
+    ),
+    home: new MyHomePage(title: 'Lists'),
+    //showPerformanceOverlay: true,
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Andrea\'s Lists',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting
-        // the app, try changing the primarySwatch below to Colors.green
-        // and then invoke "hot reload" (press "r" in the console where
-        // you ran "flutter run", or press Run > Hot Reload App in IntelliJ).
-        // Notice that the counter didn't reset back to zero -- the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new MyHomePage(title: 'Andrea\'s Lists'),
-      //showPerformanceOverlay: true,
-    );
-  }
-}
+//////
+// For Home Page that displays list and schedule
+//////
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
@@ -46,7 +42,8 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin {
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
   final List<Tab> myTabs = <Tab>[
     new Tab(
       text: 'Lists',
@@ -57,6 +54,9 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
       icon: new Icon(Icons.event),
     )
   ];
+
+  Map<EventItem, List<MyListItem>> listItems = new Map<EventItem,
+      List<MyListItem>>(); // Stores list items for every list
 
   List<EventItem> myLists = [
     new EventItem(title: "Hello", date: new DateTime.now()),
@@ -71,10 +71,15 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
 
   TabController _tabController;
 
+  void _handleListItemsChanged(EventItem event, List<MyListItem> list) {
+    listItems[event] = list;
+  }
+
   @override
   void initState() {
     super.initState();
     _tabController = new TabController(vsync: this, length: myTabs.length);
+    // TODO:
   }
 
   @override
@@ -96,55 +101,46 @@ class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateM
         // Here we take the value from the MyHomePage object that
         // was created by the App.build method, and use it to set
         // our appbar title.
-        title: new Text(config.title),
+        title: new Text(widget.title),
         bottom: new TabBar(
           controller: _tabController,
           tabs: myTabs,
         ),
       ),
       body: new TabBarView(
-          controller: _tabController,
-          children: <Widget> [
-            new MyLists(myLists),
-            new MySchedule(mySchedule),
-          ],
+        controller: _tabController,
+        children: <Widget>[
+          new MyLists(myLists, listItems, _handleListItemsChanged),
+          new MySchedule(mySchedule),
+        ],
       ),
-        floatingActionButton: new FloatingActionButton(
-            onPressed: floatingActionButtonOnPress(),
-            child: new Icon(Icons.add),
-        ),
-    );
-  }
+      floatingActionButton: new FloatingActionButton(
+        onPressed: () async {
+          EventItem newListName =
+              await Navigator.of(context).push(new MaterialPageRoute<EventItem>(
+            builder: (BuildContext context) {
+              return new NewListPage();
+            },
+          ));
 
-  void floatingActionButtonOnPress() {
-    // TODO: Implement onPressed
+          if (newListName != null) {
+            setState(() {
+              myLists.add(newListName);
+            });
+          }
+        },
+        child: new Icon(Icons.add),
+      ),
+    );
   }
 }
 
 class EventItem {
-  EventItem({@required this.title, this.date});
+  const EventItem({this.title, this.date});
 
-  String title;
-  DateTime date;
-
-  bool hasDate() {
-    return date == null;
-  }
-
-  void setDate(DateTime date) {
-    this.date = date;
-  }
+  final String title;
+  final DateTime date;
 }
-
-/*
-class MySchedule extends StatefulWidget {
-  MySchedule({Key key, this.lists}) : super(key: key);
-
-  final List<EventItem> schedule;
-
-  @override
-  _MyScheduleState createState() => new _MyScheduleState();
-}*/
 
 class MySchedule extends StatelessWidget {
   MySchedule(this.schedule);
@@ -152,21 +148,22 @@ class MySchedule extends StatelessWidget {
   List<EventItem> schedule;
 
   List<Widget> getTodaySchedule() {
-    List<Widget> todaySchedule = [new ListTile(title: new Center(child: new Text("Today")))];
+    List<Widget> todaySchedule = [
+      new ListTile(title: new Center(child: new Text("Today")))
+    ];
 
-    todaySchedule.addAll(
-        schedule.map((EventItem event) {
-          if(event.date.day == new DateTime.now().day &&
-              event.date.month == new DateTime.now().month &&
-              event.date.year == new DateTime.now().year) {
-            return new ListTile(
-                title: new Text(event.title),
-                subtitle: new Text("${event.date.month}/${event.date.day}/${event.date.year}"),
-                onTap: null,
-            );
-          }
-        }).toList()
-    );
+    todaySchedule.addAll(schedule.map((EventItem event) {
+      if (event.date.day == new DateTime.now().day &&
+          event.date.month == new DateTime.now().month &&
+          event.date.year == new DateTime.now().year) {
+        return new ListTile(
+          title: new Text(event.title),
+          subtitle: new Text(
+              "${event.date.month}/${event.date.day}/${event.date.year}"),
+          onTap: null,
+        );
+      }
+    }).toList());
 
     return todaySchedule;
   }
@@ -186,19 +183,307 @@ class MySchedule extends StatelessWidget {
 }
 
 class MyLists extends StatelessWidget {
-  MyLists(this.myLists);
+  MyLists(this.myLists, this.listItems, this.onListItemChanged);
 
   List<EventItem> myLists;
+  Map<EventItem, List<MyListItem>> listItems;
+  ListItemsChangedCallback onListItemChanged;
 
+  @override
   Widget build(BuildContext context) {
     return new ListView(
       children: myLists.map((EventItem event) {
         return new ListTile(
           title: new Text(event.title),
-          subtitle: new Text("${event.date.month}/${event.date.day}/${event.date.year}"),
-          onTap: null,
+          subtitle: new Text(
+              "${event.date.month}/${event.date.day}/${event.date.year}"),
+          onTap: () async {
+            List<MyListItem> newListOfList = await Navigator
+                .of(context)
+                .push(new MaterialPageRoute<List<MyListItem>>(
+              builder: (BuildContext context) {
+                return new ListPage(
+                  listItems: /*itemLists[event]*/ <MyListItem>[
+                    new MyListItem(title: "Hello"),
+                    new MyListItem(title: "World"),
+                    new MyListItem(title: "Hello"),
+                  ],
+                  listTitle: event.title,
+                );
+              },
+            ));
+
+            onListItemChanged(event, newListOfList);
+          },
         );
       }).toList(),
+    );
+  }
+}
+
+//////
+// Page for making new list
+//////
+
+class NewListPage extends StatefulWidget {
+  @override
+  _NewListPageState createState() => new _NewListPageState();
+}
+
+class _NewListPageState extends State<NewListPage> {
+  String _newListName;
+  List<String> _savedListNames = <String>[
+    "Don't Forget",
+    "Shopping",
+    "To-Do",
+    "Work"
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    // get _savedListNames from memory
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> getListWidgets() {
+      List<Widget> widgets = <Widget>[
+        new Text(
+          "CUSTOM LIST",
+          style: new TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.grey[500],
+          ),
+        ),
+        new Card(
+            child: new Container(
+                padding: new EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+                child: new Column(children: <Widget>[
+                  new TextField(
+                    onChanged: (String value) {
+                      _newListName = value;
+                    },
+                  ),
+                  new Builder(
+                    builder: (BuildContext context) {
+                      return new RaisedButton(
+                        child: new Text("CREATE LIST"),
+                        onPressed: () {
+                          if (_newListName != null) {
+                            Navigator.of(context).pop(new EventItem(
+                                title: _newListName, date: new DateTime.now()));
+                          } else {
+                            Scaffold.of(context).showSnackBar(new SnackBar(
+                                  content: new Text("Your list needs a name"),
+                                ));
+                          }
+                        },
+                      );
+                    },
+                  )
+                ]))),
+        new Text(
+          "QUICK LIST",
+          style: new TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.grey[500],
+          ),
+        ),
+      ];
+
+      widgets.add(new Card(
+        child: new Column(
+          children: _savedListNames.map((String name) {
+            return new ListTile(
+              title: new Text(name),
+              onTap: () {
+                Navigator
+                    .of(context)
+                    .pop(new EventItem(title: name, date: new DateTime.now()));
+              },
+            );
+          }).toList(),
+        ),
+      ));
+
+      return widgets;
+    }
+
+    return new Scaffold(
+      appBar: new AppBar(
+        leading: new IconButton(
+          icon: new Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop(null);
+          },
+        ),
+        title: new Text('New List'),
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.save_alt),
+            onPressed: () {
+              if (!_savedListNames.contains(_newListName) &&
+                  _savedListNames != null) {
+                setState(() {
+                  _savedListNames.add(_newListName);
+                });
+              }
+            },
+          ),
+        ],
+      ),
+      body: new ListView(
+        padding: new EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
+        children: getListWidgets(),
+      ),
+    );
+  }
+} 
+
+//TODO: Everything below this comment (It creates a page that allows the user to view the items within their individual lists
+// NOTE: Code below is mostly borrowed from https://flutter.io/widgets-intro/ .
+// This tutorial includes system for changing list items' states
+
+class MyListItem {
+  const MyListItem({this.title});
+  final String title;
+}
+
+typedef void ListChangedCallback(MyListItem item, bool isDone);
+typedef void ListItemsChangedCallback(EventItem event, List<MyListItem> list);
+
+class MyListItemWidget extends StatelessWidget {
+  MyListItemWidget({MyListItem item, this.isDone, this.onListChanged})
+      : item = item,
+        super(key: new ObjectKey(item));
+
+  final MyListItem item;
+  final bool isDone;
+  final ListChangedCallback onListChanged;
+
+  Color _getColor(BuildContext context) {
+    return isDone ? Colors.black54 : Theme.of(context).primaryColor;
+  }
+
+  TextStyle _getTextStyle(BuildContext context) {
+    if (!isDone) return null;
+
+    return new TextStyle(
+      color: Colors.black54,
+      decoration: TextDecoration.lineThrough,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new ListTile(
+      onTap: () {
+        onListChanged(item, !isDone);
+      },
+      leading: new CircleAvatar(
+        backgroundColor: _getColor(context),
+        child: new Text(item.title[0]),
+      ),
+      title: new Text(item.title, style: _getTextStyle(context)),
+    );
+  }
+}
+
+class ListPage extends StatefulWidget {
+  ListPage({Key key, this.listItems, this.listTitle}) : super(key: key);
+
+  final List<MyListItem> listItems;
+  final String listTitle;
+
+  @override
+  _ListPageState createState() => new _ListPageState();
+}
+
+class _ListPageState extends State<ListPage> {
+  String _newListItemName;
+  List<MyListItem> myListItems;
+  Set<MyListItem> _list = new Set<MyListItem>();
+
+  void _handleListChanged(MyListItem item, bool isDone) {
+    setState(() {
+      if (isDone)
+        _list.add(item);
+      else
+        _list.remove(item);
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    myListItems = widget.listItems;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> widgets = <Widget>[
+      new Card(
+          child: new Container(
+              padding: new EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 16.0),
+              child: new Column(children: <Widget>[
+                new TextField(
+                  onChanged: (String value) {
+                    _newListItemName = value;
+                  },
+                ),
+                new Builder(
+                  builder: (BuildContext context) {
+                    return new RaisedButton(
+                      child: new Text("ADD"),
+                      onPressed: () {
+                        if (_newListItemName != null) {
+                          setState(() {
+                            myListItems
+                                .add(new MyListItem(title: _newListItemName));
+                          });
+                        } else {
+                          Scaffold.of(context).showSnackBar(new SnackBar(
+                                content: new Text("Your list needs a name"),
+                              ));
+                        }
+                      },
+                    );
+                  },
+                )
+              ]))),
+    ];
+
+    widgets.addAll(myListItems.map((MyListItem item) {
+      return new MyListItemWidget(
+        item: item,
+        isDone: _list.contains(item),
+        onListChanged: _handleListChanged,
+      );
+    }).toList());
+
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.listTitle),
+        leading: new IconButton(
+          icon: new Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop(null);
+          },
+        ),
+        actions: <Widget>[
+          new IconButton(
+            icon: new Icon(Icons.save),
+            onPressed: () {
+              Navigator.of(context).pop(myListItems);
+            },
+          ),
+        ],
+      ),
+      body: new ListView(
+        padding: new EdgeInsets.symmetric(vertical: 8.0),
+        children: widgets,
+      ),
     );
   }
 }
